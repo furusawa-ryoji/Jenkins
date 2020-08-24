663719f08df8cc576ce9e3a8ee2e0f99ec3a0169
rfurusawa <kingdomheartsmickey@gmail.com>
2020-08-24 23:02:03 +0900

    file作成

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -1,10 +1,14 @@
 import unittest
 import main
 
+import enum
+import pathlib
+from datetime import datetime
+
 class TestCalc(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        print("set up class")
+    # @classmethod
+    # def setUpClass(self):
+    #     print("set up class")
         
     def test_login_s01(self):
         self.assertEqual(main.title1,'ポータル')
@@ -15,9 +19,36 @@ class TestCalc(unittest.TestCase):
     def test_login_s03(self):
         self.assertEqual(main.title3,'トップページ - サイボウズ Office')
     
-    @classmethod
-    def tearDownClass(self):
-        print("tear down class")
+    # @classmethod
+    # def tearDownClass(self):
+    #     print("tear down class")
         
+def _main():
+    """The main process at script execution."""
+    @enum.unique
+    class Verbosity(enum.IntEnum):
+        NOTHING = 0
+        SIMPLE = 1
+        DETAIL = 2
+
+    CURRENT_PATH = pathlib.Path('result.txt')
+    EXEC_DATETIME = datetime.today()
+    FILE_MOD_DATETIME = datetime.fromtimestamp(CURRENT_PATH.stat().st_mtime)
+    LOG_PATH = CURRENT_PATH.with_name(
+        CURRENT_PATH.stem + f'_{EXEC_DATETIME.date()}.log')
+
+    with open(pathlib.Path(LOG_PATH), 'w') as fw:
+        fw.write(
+            f'[Date of script modification] {str(FILE_MOD_DATETIME)}\n'
+            f'[Date of this test execution] {str(EXEC_DATETIME)}\n'
+            '\n')
+
+        unittest.main(
+            testRunner=unittest.TextTestRunner(
+                stream=fw,
+                descriptions=False,
+                verbosity=Verbosity.DETAIL))
+
+
 if __name__ == '__main__':
-    unittest.main()
+    _main()
